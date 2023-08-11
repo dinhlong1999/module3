c633588168e027b29c6e7556a55681ea6417d8e7
dinhlong1999 <long.truong.vcr@gmail.com>
2023-08-11 10:49:48 +0700

    longnopbai

--- ss01/exercise/exercise1.sql
+++ ss01/exercise/exercise1.sql
@@ -2,19 +2,19 @@ create database student_management ;
 
 use student_management;
 
-create table Teacher(
-id int primary key auto_increment,
+create table teacher(
+`id` int primary key auto_increment,
 `name` varchar(200),
-age int,
-country varchar(50)
+`age` int,
+`country` varchar(50)
 );
 
-create table Class(
-id int primary key auto_increment,
+create table class(
+`id` int primary key auto_increment,
 `name` varchar(200)
 );
 
-insert into teacher(`name`, age, country)
+insert into teacher(`name`, `age`, `country`)
 values ("Hau",19,"VN"),("long",19,"VN"),("Thien",19,"VN");
 
 
@@ -25,7 +25,9 @@ values ("codegym");
 
 set sql_safe_updates = 0;
 update class
-set `name` = "pct"
-where `name` = "codegym";
+set `name` = "hd"
+where `name` = "pct";
 set sql_safe_updates = 1;
+
+
 
