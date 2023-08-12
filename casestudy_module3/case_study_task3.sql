28b25ce9ad5f1e885c730b96d29f681f40b5e530
dinhlong1999 <long.truong.vcr@gmail.com>
2023-08-12 09:55:54 +0700

    longnopbai

---
+++ casestudy_module3/case_study_task3.sql
@@ -0,0 +1,63 @@
+select * from loai_dich_vu;
+
+insert into loai_dich_vu(ten_loai_dich_vu)
+values ("Villa"),("House"),("Room");
+
+INSERT INTO dich_vu (ten_dich_vu, dien_tich, chi_phi_thue, so_nguoi_toi_da, tieu_chuan_phong, mo_ta_tien_nghi_khac, dien_tich_ho_boi, so_tang, ma_kieu_thue, ma_loai_dich_vu)
+VALUES
+('Villa Beach Front', 25000, 10000000, 10, 'vip', 'Có hồ bơi', 500, 4, 3, 1),
+('House Princess 01', 14000, 5000000, 7, 'vip', 'Có thêm bếp nướng', null, 3, 2, 2),
+('Room Twin 01', 5000, 1000000, 2, 'normal', 'Có tivi', null, null, 4, 3),
+('Villa No Beach Front', 22000, 9000000, 8, 'normal', 'Có hồ bơi', 300, 3, 3, 1),
+('House Princess 02', 10000, 4000000, 5, 'normal', 'Có thêm bếp nướng', null, 2, 3, 2),
+('Room Twin 02', 3000, 900000, 2, 'normal', 'Có tivi', null, null, 4, 3);
+
+select * from hop_dong;
+
+INSERT INTO dich_vu_di_kem (ten_dich_vu_di_kem, gia, don_vi,trang_thai)
+VALUES
+('Karaoke', 10000, 'giờ', 'tiện nghi, hiện tại'),
+('Thuê xe máy', 10000, 'chiếc', 'hỏng 1 xe'),
+('Thuê xe đạp', 20000, 'chiếc', 'tốt'),
+('Buffet buổi sáng', 15000, 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+('Buffet buổi trưa', 90000, 'suất', 'đầy đủ đồ ăn, tráng miệng'),
+('Buffet buổi tối', 16000, 'suất', 'đầy đủ đồ ăn, tráng miệng');
+
+INSERT INTO hop_dong (ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, ma_nhan_vien, ma_khach_hang, ma_dich_vu)
+VALUES
+('2020-12-08', '2020-12-08', 0, 3, 1, 3),
+('2020-07-14', '2020-07-21', 200000, 7, 3, 1),
+('2021-03-15', '2021-03-17', 50000, 3, 4, 2),
+('2021-01-14', '2021-01-18', 100000, 7, 5, 5),
+('2021-07-14', '2021-07-15', 0, 7, 2, 6),
+('2021-06-01', '2021-06-03', 0, 7, 7, 6),
+('2021-09-02', '2021-09-05', 100000, 7, 4, 4),
+('2021-06-17', '2021-06-18', 150000, 3, 4, 1),
+('2020-11-19', '2020-11-19', 0, 3, 4, 3),
+('2021-04-12', '2021-04-14', 0, 10, 3, 5),
+('2021-04-25', '2021-04-25', 0, 2, 2, 1),
+('2021-05-25', '2021-05-27', 0, 7, 10, 1);
+
+
+insert into hop_dong_chi_tiet(so_luong,ma_hop_dong,ma_dich_vu_di_kem)
+values( 5,2,4),
+(8,2,5),
+(15,2,6),
+(1,3,1),
+(11,3,2),
+(1,1,3),
+(2,1,2),
+(2,12,2);
+select * from hop_dong_chi_tiet;
+
+
+
+
+
+
+
+
+
+
+
+
